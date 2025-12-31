cade1a8367cadeb4d679af80c3a4a96b12df48ca
Souvik <souvikelric@gmail.com>
2025-12-31 21:00:53 +0530

    Added basic python code

--- commit-checker.py
+++ commit-checker.py
@@ -4,8 +4,7 @@ url = "https://api.github.com/users/souvikelric/repos"
 
 response = requests.get(url)
 data = response.json()
-comm_url = data[0]["commits_url"]
+for val in data:
+    print(val["name"])
 
-comm_response = requests.get(comm_url)
-comm_data = comm_response.json()
-print(comm_data)
+comm_url = data[0]["commits_url"]
