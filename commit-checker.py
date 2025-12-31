dcc6d4f6cfb16678ed37e7fcaf7ac3f05295c579
Souvik <souvikelric@gmail.com>
2025-12-31 21:06:28 +0530

    Added basic python code

--- commit-checker.py
+++ commit-checker.py
@@ -4,7 +4,14 @@ url = "https://api.github.com/users/souvikelric/repos"
 
 response = requests.get(url)
 data = response.json()
+private_repos = 0
 for val in data:
-    print(val["name"])
+    print(val["private"])
+    if val["private"]:
+        private_repos += 1
 
+print("\033[33mNumber of repositories : " + str(len(data)) + "\033[0m")
+print("\033[33mNumber of private repositories : " + str(private_repos) + "\033[0m")
+
+print(data[0]["private"])
 comm_url = data[0]["commits_url"]
